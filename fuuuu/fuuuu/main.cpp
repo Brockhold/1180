20fde5262c81cdb85f6adbd7485e6b393b9221a6
brockhold <ben.rockhold@gmail.com>
2015-02-03 09:01:58 -0500

    First post. Happened to get it working in Xcode by luck.

---
+++ fuuuu/fuuuu/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  first
+//
+//  Created by Ben Rockhold on 2/3/15.
+//  No rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    string first, last;
+    cout << "Please enter your first and last name." << endl;
+    cout << "Do not forget to press ENTER" << endl;
+    
+    cin >> first >> last;
+    
+    cout << "\nThis is my first program!" << endl;
+    cout << "My name is: " << first << " " << last << endl;
+    
+    return 0;
+} // end main}
