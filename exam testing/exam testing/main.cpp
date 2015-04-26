5b614ba81de2878e259ba6a9a9a9a577999e9f8b
brockhold <ben.rockhold@gmail.com>
2015-04-26 18:34:48 -0400

    minor and partly unknown update to the exam testing program

--- exam testing/exam testing/main.cpp
+++ exam testing/exam testing/main.cpp
@@ -7,9 +7,53 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//int main() {
+//    int count = 0, sum = 0, i, a;
+//    for(i = 1; i <=5; i++){
+//        cin >> a;
+//        count++;
+//        sum = sum+a;
+//    }
+//    cout << sum << " " << count << " " << a << endl;
+//    return 0;
+//}
+
+//int main(){
+//    int x=5,y=4;
+//    cout << x << " " << y << endl;
+//    while(x >= 0){
+//        if(x==y){
+//            x = x -y;
+//        }
+//        x--;
+//        cout << x << " " << y << endl;
+//    }
+//    cout << x +1 << " " << y << y * 2 << endl;
+//    cout << x << " " << y << endl;
+//    return 0;
+//}
+
+//int main(){
+//    int b=2,c=6,d=5,f=2,x=5;
+//    x = 2+c%b*-d/f+1;
+//    cout << x;
+//    return 0;
+//}
+
+
+int main(){
+    int a,b,c,d=0;
+    int changes (int,int,int);
+    a=3;
+    b=-5;
+    c=2;
+    d= changes(a,b,c);
+    cout << d;
     return 0;
 }
+
+
+
+
